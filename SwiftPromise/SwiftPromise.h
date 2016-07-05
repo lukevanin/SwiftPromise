90fb40f70fe6fed22a8379fddd1b30499bfb529c
Luke Van In <lukevanin@gmail.com>
2016-07-05 22:35:59 +0200

    Initial Commit

---
+++ SwiftPromise/SwiftPromise.h
@@ -0,0 +1,19 @@
+//
+//  SwiftPromise.h
+//  SwiftPromise
+//
+//  Created by Luke Van In on 2016/07/05.
+//  Copyright © 2016 Luke Van In. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftPromise.
+FOUNDATION_EXPORT double SwiftPromiseVersionNumber;
+
+//! Project version string for SwiftPromise.
+FOUNDATION_EXPORT const unsigned char SwiftPromiseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftPromise/PublicHeader.h>
+
+
